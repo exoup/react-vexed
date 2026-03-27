b6a501638cf348853c407c8bf83e3628bebbbc7a
Joseph <josephy@dram.space>
2026-03-26 22:58:17 -0500

    Game board

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,11 @@
 import "./index.css";
-import GemBlock from "@/components/Game/GemBlock";
-import BoundaryBlock from "@/components/Game/BoundaryBlock";
-import { gemColors, blockSize } from "@/lib/constants";
-
-const levelMap = [
-  [1, 1, 1, 1, 1, 1],
-  [1, 7, 8, 0, 0, 1],
-  [1, 1, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 1],
-  [1, 8, 0, 0, 7, 1],
-  [1, 1, 8, 7, 1, 1],
-  [1, 1, 1, 1, 1, 1],
-];
+import Board from "@/components/Game/Board";
 
 export function App() {
-  const mapBlocks = levelMap.flatMap((row, y) =>
-    row.flatMap((cell, x) => {
-      if (!cell) return null;
-
-      if (cell === 1) return (
-        <BoundaryBlock
-          key={`boundary-${x}-${y}`}
-          x={x}
-          y={y}
-          levelMap={levelMap}
-        />
-      );
-
-      if (cell > 1) return (
-        <GemBlock
-          color={gemColors[cell - 2]}
-          id={`${cell}-${x}-${y}`}
-          key={`gem-${x}-${y}`}
-          x={x}
-          y={y}
-        />
-      );
-    }),
-  );
 
   return (
     <div className="min-h-screen bg-game-background text-neutral-800 p-4 font-sans flex flex-col items-center justify-center">
-
-      <div
-        className="relative bg-transparent border-t border-l border-t-boundary-edge/50 border-l-boundary-edge/50 shadow-2xl rounded-xs overflow-clip select-none touch-none"
-        style={{ width: levelMap[0]!.length * blockSize, height: levelMap.length * blockSize }}
-      >
-        {mapBlocks}
-      </div>
-
+      <Board />
     </div>
   );
 }
